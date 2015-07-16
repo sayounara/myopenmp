5e3110a73a80c8cbd64dac6acffae21cf8a14258
sayounara <hexin@hexin-ThinkPad-Edge-E531>
2015-07-16 21:37:33 +0800

    first commit myopenmp

---
+++ src/openmp.c
@@ -0,0 +1,23 @@
+/*
+ * openmp.c
+ *
+ *  Created on: 2015年7月16日
+ *      Author: hexin
+ */
+#include <omp.h>
+#include<stdio.h>
+//int main()
+//{
+//		int k=100;
+//		int data[10]={5,3,7,7,95,8,5,3,79,85};
+//		int i=0;
+//		int max=0;
+//		#pragma omp parallel for
+//		for(i=0;i<10;i++)
+//		{
+//			if(data[i]>max) max=data[i];
+//			printf("now max=%d, and TID=%d\n",max,omp_get_thread_num());
+//		}
+//		printf("final max=%d\n",max);
+//}
+
